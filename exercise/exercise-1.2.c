4dce49a590c394a1ad1d63dc1f4f3bb86164c588
lujinjianst <lu605894472@gmail.com>
2012-11-23 22:58:17 +0800

    lesson 1

--- exercise/exercise-1.2.c
+++ exercise/exercise-1.2.c
@@ -1,20 +1,17 @@
 #include <stdio.h>
-#include <stdlib.h>
 
 void main(void)
  {
    unsigned value = 1;
 
-   printf("%u rotated right once is %u\n", value,
-     _rotr(value, 1));
+   printf("%x\n", value);
 
    value = 5;
-   printf("%u rotated right twice is %u\n", value,
-     _rotr(value, 2));
+   printf("%x\n", value);
 
    value = 65534;
-   printf("%u rotated left twice is %u\n", value,
-     _rotl(value, 2));
+   printf("%x\n", value);
+
  }
 
 
