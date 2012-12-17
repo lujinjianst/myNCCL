353897322be51486f7691a94d136fda74681a05f
lujinjianst <lu605894472@gmail.com>
2012-12-17 21:04:44 +0800

    school-management-system's architecture design

---
+++ practices/School-Management-System/school.c
@@ -0,0 +1,77 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define M 3
+
+void man_interface();
+void find_interface();
+
+void choice(long sno[], char sname[][20], int sage[], float cs[], int N);
+void find_choice(long sno[], char sname[][20], int sage[], float cs[], int N);
+void input(long sno[], char sname[][20], int sage[], float cs[], int N);
+void output(long sno[], char sname[][20], int sage[], float cs[], int N);
+void sno_find(long sno[], char sname[][20], int sage[], float cs[], int N);
+void sname_find(long sno[], char sname[][20], int sage[], float cs[], int N);
+void cs_sort(long sno[], char sname[][20], int sage[], float cs[], int N);
+void total(long sno[], char sname[][20], int sage[], float cs[], int N);
+
+
+int main(int argc, const char *argv[])
+{
+	long sno[M];
+	char sname[M][20];
+	int sage[M];
+	float cs[M];
+	int i, j;
+
+	main_interface();
+
+	choice(sno, sname, sage, cs, M);
+	return 0;
+}
+
+void man_interface()
+{
+
+}
+void find_interface()
+{
+
+}
+void choice(long sno[], char sname[][20], int sage[], float cs[], int N)
+{
+
+}
+void find_choice(long sno[], char sname[][20], int sage[], float cs[], int N)
+{
+	
+}
+void input(long sno[], char sname[][20], int sage[], float cs[], int N)
+{
+
+}
+void output(long sno[], char sname[][20], int sage[], float cs[], int N)
+{
+
+}
+void sno_find(long sno[], char sname[][20], int sage[], float cs[], int N)
+{
+
+}
+void sname_find(long sno[], char sname[][20], int sage[], float cs[], int N)
+{
+
+}
+void cs_sort(long sno[], char sname[][20], int sage[], float cs[], int N)
+{
+
+}
+void total(long sno[], char sname[][20], int sage[], float cs[], int N)
+{
+
+}
+
+
+
+
